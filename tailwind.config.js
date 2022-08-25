9b6397ab9b410599b3aea271710e81013ff1cffc
Aman Kumar Prasad <amankumarprasad72@gmail.com>
2022-08-25 13:02:17 +0530

    This is homepage

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["*"],
+    theme: {
+        extend: {
+            fontFamily: {
+                Inter: 'Raleway'
+            },
+            animation:{
+                slide:'ping 5s cubic-bezier(1, 1, 0.2, 1) infinite',
+            },
+          
+        },
+    },
+    plugins: [],
+}
